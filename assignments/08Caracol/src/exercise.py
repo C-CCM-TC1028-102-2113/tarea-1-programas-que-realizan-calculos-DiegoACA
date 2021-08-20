6e6427a4c9d8ad011eb7334cbbf1c58c6cae6db3
DiegoACA <88989875+DiegoACA@users.noreply.github.com>
2021-08-20 17:21:23 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,13 +2,11 @@ def main():
     #escribe tu código abajo de esta línea
 
     vel = float(5.7)
-    min = float(1)
+    min = float(input("Dame los minutos:"))
 
     dis = (vel * (min*60))/10
 
     print("Centímentros recorridos:", dis)
-    
-pass
-
+   
 if __name__ == '__main__':
     main()
